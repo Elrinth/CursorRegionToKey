048192d184718b59fd3ff0ad4e72a2811ebd5a86
Nicht Und Zwei <elrinth@gmail.com>
2022-02-18 19:27:38 +0100

    Commiting initial commit

---
+++ MouseCursorScreenMapProgram/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace MouseCursorScreenMapProgram
+{
+  partial class CursorRegionToKey
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.mouseX = new System.Windows.Forms.Label();
+      this.mouseY = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.labelTop = new System.Windows.Forms.Label();
+      this.labelBottom = new System.Windows.Forms.Label();
+      this.labelLeft = new System.Windows.Forms.Label();
+      this.labelRight = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // mouseX
+      // 
+      this.mouseX.AutoSize = true;
+      this.mouseX.Location = new System.Drawing.Point(541, 13);
+      this.mouseX.Name = "mouseX";
+      this.mouseX.Size = new System.Drawing.Size(32, 13);
+      this.mouseX.TabIndex = 0;
+      this.mouseX.Text = "mx: 0";
+      // 
+      // mouseY
+      // 
+      this.mouseY.AutoSize = true;
+      this.mouseY.Location = new System.Drawing.Point(541, 26);
+      this.mouseY.Name = "mouseY";
+      this.mouseY.Size = new System.Drawing.Size(32, 13);
+      this.mouseY.TabIndex = 1;
+      this.mouseY.Text = "my: 0";
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 10;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // labelTop
+      // 
+      this.labelTop.AutoSize = true;
+      this.labelTop.Location = new System.Drawing.Point(280, 9);
+      this.labelTop.Name = "labelTop";
+      this.labelTop.Size = new System.Drawing.Size(22, 13);
+      this.labelTop.TabIndex = 2;
+      this.labelTop.Text = "top";
+      this.labelTop.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // labelBottom
+      // 
+      this.labelBottom.AutoSize = true;
+      this.labelBottom.Location = new System.Drawing.Point(280, 299);
+      this.labelBottom.Name = "labelBottom";
+      this.labelBottom.Size = new System.Drawing.Size(39, 13);
+      this.labelBottom.TabIndex = 3;
+      this.labelBottom.Text = "bottom";
+      // 
+      // labelLeft
+      // 
+      this.labelLeft.AutoSize = true;
+      this.labelLeft.Location = new System.Drawing.Point(12, 146);
+      this.labelLeft.Name = "labelLeft";
+      this.labelLeft.Size = new System.Drawing.Size(21, 13);
+      this.labelLeft.TabIndex = 4;
+      this.labelLeft.Text = "left";
+      // 
+      // labelRight
+      // 
+      this.labelRight.AutoSize = true;
+      this.labelRight.Location = new System.Drawing.Point(590, 146);
+      this.labelRight.Name = "labelRight";
+      this.labelRight.Size = new System.Drawing.Size(27, 13);
+      this.labelRight.TabIndex = 5;
+      this.labelRight.Text = "right";
+      // 
+      // CursorRegionToKey
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(624, 321);
+      this.Controls.Add(this.labelRight);
+      this.Controls.Add(this.labelLeft);
+      this.Controls.Add(this.labelBottom);
+      this.Controls.Add(this.labelTop);
+      this.Controls.Add(this.mouseY);
+      this.Controls.Add(this.mouseX);
+      this.DoubleBuffered = true;
+      this.Name = "CursorRegionToKey";
+      this.Text = "Form1";
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+      this.Resize += new System.EventHandler(this.Form1_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label mouseX;
+    private System.Windows.Forms.Label mouseY;
+    private System.Windows.Forms.Timer timer1;
+    private System.Windows.Forms.Label labelTop;
+    private System.Windows.Forms.Label labelBottom;
+    private System.Windows.Forms.Label labelLeft;
+    private System.Windows.Forms.Label labelRight;
+  }
+}
+
---
+++ MouseCursorScreenMapProgram/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace MouseCursorScreenMapProgram
+{
+  public partial class CursorRegionToKey : Form
+  {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct KeyboardInput
+    {
+      public ushort wVk;
+      public ushort wScan;
+      public uint dwFlags;
+      public uint time;
+      public IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MouseInput
+    {
+      public int dx;
+      public int dy;
+      public uint mouseData;
+      public uint dwFlags;
+      public uint time;
+      public IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HardwareInput
+    {
+      public uint uMsg;
+      public ushort wParamL;
+      public ushort wParamH;
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    public struct InputUnion
+    {
+      [FieldOffset(0)] public MouseInput mi;
+      [FieldOffset(0)] public KeyboardInput ki;
+      [FieldOffset(0)] public HardwareInput hi;
+    }
+    public struct Input
+    {
+      public int type;
+      public InputUnion u;
+    }
+    [Flags]
+    public enum InputType
+    {
+      Mouse = 0,
+      Keyboard = 1,
+      Hardware = 2
+    }
+
+    [Flags]
+    public enum KeyEventF
+    {
+      KeyDown = 0x0000,
+      ExtendedKey = 0x0001,
+      KeyUp = 0x0002,
+      Unicode = 0x0004,
+      Scancode = 0x0008
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint SendInput(uint nInputs, Input[] pInputs, int cbSize);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetMessageExtraInfo();
+
+    //DateTime lastTime = DateTime.Now;
+    //int refreshEllapse = (int)1000 / 10;
+    private Rectangle rectangle = new Rectangle(0,0,0,0);
+    private Rectangle cursorPosition = new Rectangle(0, 0, 16, 16);
+    private bool pDown = false;
+    private bool pUp = false;
+    private bool pLeft = false;
+    private bool pRight = false;
+
+    const int VK_UP = 0x26; //up key
+    const int VK_DOWN = 0x28;  //down key
+    const int VK_LEFT = 0x25;
+    const int VK_RIGHT = 0x27;
+
+    const ushort K_S = 0x1F;
+    const ushort K_W = 0x11;
+    const ushort K_A = 0x1E;
+    const ushort K_D = 0x20;
+    
+    const ushort K_UP = 0xC8;
+    const ushort K_LEFT = 0xCB;
+    const ushort K_RIGHT = 0xCD;
+    const ushort K_DOWN = 0xD0;
+
+    const uint KEYEVENTF_KEYUP = 0x0002;
+    const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+    const uint WM_KEYDOWN = 0x0100;
+    const uint WM_KEYUP = 0x0101;
+    protected Process p = null;
+
+    public CursorRegionToKey()
+    {
+      InitializeComponent();
+      rectangle = new Rectangle(this.ClientSize.Width / 2 - (this.ClientSize.Width / 4), this.ClientSize.Height / 2 - (this.ClientSize.Height / 4), (this.ClientSize.Width / 2), (this.ClientSize.Height / 2));
+      
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g = e.Graphics;
+      g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+      g.Clear(BackColor);
+      Pen pen1 = new Pen(Color.Black, 1);
+      //surface.DrawLine(pen1, 0, 0, this.ClientSize.Width / 2, this.ClientSize.Height);
+      g.DrawRectangle(pen1, rectangle);
+
+      Cursor.Draw(g, cursorPosition);
+
+      mouseX.Text = "mx: " + Cursor.Position.X;
+      mouseY.Text = "my: " + Cursor.Position.Y;
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      // recalculate the form size...
+      rectangle = new Rectangle(this.ClientSize.Width / 2 - (this.ClientSize.Width / 4), this.ClientSize.Height / 2 - (this.ClientSize.Height / 4), (this.ClientSize.Width / 2), (this.ClientSize.Height / 2));
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      var screenSize = Screen.PrimaryScreen.Bounds.Size;
+      // calculate mouse poosition:
+      float resultX = (float)Cursor.Position.X / (float)screenSize.Width;
+      float resultY = (float)Cursor.Position.Y / (float)screenSize.Height;
+      cursorPosition.X = (int)(resultX * this.ClientSize.Width);
+      cursorPosition.Y = (int)(resultY * this.ClientSize.Height);
+
+      // check directions:
+      checkDirections();
+
+      sendKeyboardCommands();
+
+      Invalidate();
+    }
+
+    private void sendMoveCommand(ushort iKey, KeyEventF eventF)
+    {
+      Input[] inputs = new Input[]
+        {
+            new Input
+            {
+                type = (int)InputType.Keyboard,
+                u = new InputUnion
+                {
+                    ki = new KeyboardInput
+                    {
+                        wVk = 0,
+                        wScan = iKey,
+                        dwFlags = (uint)(eventF | KeyEventF.Scancode),
+                        dwExtraInfo = GetMessageExtraInfo()
+                    }
+                }
+            }
+        };
+
+      SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(Input)));
+    }
+
+    private void sendKeyboardCommands()
+    {
+      if (pDown)
+      {
+        sendMoveCommand(K_S, KeyEventF.KeyDown);
+      }
+      if (pUp)
+      {
+        sendMoveCommand(K_W, KeyEventF.KeyDown);
+      }
+      if (pLeft)
+      {
+        sendMoveCommand(K_A, KeyEventF.KeyDown);
+      }
+
+      if (pRight)
+      {
+        sendMoveCommand(K_D, KeyEventF.KeyDown);
+      }
+    }
+
+    private void checkDirections()
+    {
+      // down
+      if (cursorPosition.Y > rectangle.Y + rectangle.Height)
+      {
+        if (pDown == false)
+        {
+          pDown = true;
+          labelBottom.Font = new Font(labelBottom.Font, System.Drawing.FontStyle.Bold);
+        }
+      }
+      else
+      {
+        if (pDown == true)
+        {
+          pDown = false;
+          labelBottom.Font = new Font(labelBottom.Font, System.Drawing.FontStyle.Regular);
+          sendMoveCommand(K_S, KeyEventF.KeyUp);
+        }
+      }
+
+
+      // up
+      if (cursorPosition.Y < rectangle.Y)
+      {
+        if (pUp == false)
+        {
+          pUp = true;
+          labelTop.Font = new Font(labelTop.Font, System.Drawing.FontStyle.Bold);
+        }
+      }
+      else
+      {
+        if (pUp == true)
+        {
+          pUp = false;
+          labelTop.Font = new Font(labelTop.Font, System.Drawing.FontStyle.Regular);
+          sendMoveCommand(K_W, KeyEventF.KeyUp);
+        }
+      }
+
+      // left
+      if (cursorPosition.X < rectangle.X)
+      {
+        if (pLeft == false)
+        {
+          pLeft = true;
+          labelLeft.Font = new Font(labelLeft.Font, System.Drawing.FontStyle.Bold);
+        }
+      }
+      else
+      {
+        if (pLeft == true)
+        {
+          pLeft = false;
+          labelLeft.Font = new Font(labelLeft.Font, System.Drawing.FontStyle.Regular);
+          sendMoveCommand(K_A, KeyEventF.KeyUp);
+        }
+      }
+
+      // right
+      if (cursorPosition.X > rectangle.X + rectangle.Width)
+      {
+        if (pRight == false)
+        {
+          pRight = true;
+          labelRight.Font = new Font(labelRight.Font, System.Drawing.FontStyle.Bold);
+        }
+      }
+      else
+      {
+        if (pRight == true)
+        {
+          pRight = false;
+          labelRight.Font = new Font(labelRight.Font, System.Drawing.FontStyle.Regular);
+          sendMoveCommand(K_D, KeyEventF.KeyUp);
+        }
+      }
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
